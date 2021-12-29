6bcd981df4c7f9a368415448fcc64352305dc191
Shivam Gupta <sgupta@aptean.com>
2021-12-29 22:59:14 +0530

    set debug to false

--- imgcrypt/settings.py
+++ imgcrypt/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '=4_k@x1urszgy3zb5q^hq(4g=!%*7lcxk^e+7p(ey3nr+hok^('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
